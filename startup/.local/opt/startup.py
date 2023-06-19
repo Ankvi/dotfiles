1c8ae6f98141415bf6cd9451a81591af5172325f
Andreas Kvist <kvistcake@gmail.com>
2023-06-19 09:37:19 +0200

    Icons and only start shit if it's not already running

--- startup/.local/opt/startup.py
+++ startup/.local/opt/startup.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import subprocess
 import os.path
 import shutil
+import psutil
 
 # Get timestamp
 timestamp = datetime.now()
@@ -9,19 +10,40 @@ SATURDAY = 5
 
 # Define programs
 TEAMS_APPIMAGE = "/opt/appimages/teams-for-linux.AppImage"
-SLACK = shutil.which("slack")
-ONEPASSWORD = shutil.which("1password")
-YAKUAKE = shutil.which("yakuake")
+SLACK = "slack"
+ONEPASSWORD = "1password"
+YAKUAKE = "yakuake"
 
-if ONEPASSWORD is not None:
-    subprocess.Popen([ONEPASSWORD])
+def is_process_running(name):
+    for proc in psutil.process_iter(attrs=['pid', 'name']):
+        if proc.name() == name:
+            print(f"Process {name} is running")
+            return True
 
-if YAKUAKE is not None:
-    subprocess.Popen([YAKUAKE])
+    return False
+
+def start_appimage_if_not_running(appimage):
+    if not os.path.isfile(appimage):
+        return
+
+    basename = os.path.basename(appimage)
+    if not is_process_running(basename):
+        subprocess.Popen([appimage])
+
+def start_if_not_running(name):
+    program = shutil.which(name)
+    if program is None:
+        return
+
+    if not is_process_running(name):
+        subprocess.Popen([program])
+
+start_if_not_running(ONEPASSWORD)
+start_if_not_running(YAKUAKE)
 
 # If it's not the weekend, open work related programs
 if timestamp.weekday() < SATURDAY and timestamp.hour < 17:
-    if os.path.isfile(TEAMS_APPIMAGE):
-        subprocess.Popen([TEAMS_APPIMAGE])
-    if SLACK is not None:
-        subprocess.Popen([SLACK])
+    print("It's a work day, opening work programs")
+
+    start_appimage_if_not_running(TEAMS_APPIMAGE)
+    start_if_not_running(SLACK)
