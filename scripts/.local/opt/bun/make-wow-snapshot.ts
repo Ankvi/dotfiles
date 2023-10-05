1d59a5219e05f059ac209ed0a854ef40e7b0325d
Andreas Kvist <kvistcake@gmail.com>
2023-10-05 21:36:05 +0200

    bun scripts

---
+++ scripts/.local/opt/bun/make-wow-snapshot.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env bun
+
+const directory = `${process.env.HOME}/Games/battlenet/drive_c/"Program Files (x86)"/"World of Warcraft"`;
+
+try {
+    const now = new Date();
+    const commitMessage = `Backup: ${now.toLocaleDateString("en-GB")}`;
+
+    console.log(`Making WoW backup with message:\n${commitMessage}`);
+
+    const commitProcess = Bun.spawn(["git", "commit", "-am", commitMessage], {
+        cwd: directory
+    });
+
+    const exitCode = await commitProcess.exited;
+    if (exitCode) {
+        throw new Error(commitProcess.stderr);
+    }
+
+    await Bun.spawn(["git", "push"], {
+        cwd: directory
+    }).exited;
+} catch (error) {
+    console.error("WoW WTF backup failed with error:");
+    console.error(error);
+}
