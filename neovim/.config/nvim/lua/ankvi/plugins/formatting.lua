68a85ea18f487282bd84033f3d5264ffb10d9359
Andreas Kvist <kvistcake@gmail.com>
2023-10-21 20:40:26 +0200

    Replace null-ls

---
+++ neovim/.config/nvim/lua/ankvi/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                markdown = { "prettierd" },
+                javascript = { "prettierd" },
+                javascriptreact = { "prettierd" },
+                typescript = { "prettierd" },
+                typescriptreact = { "prettierd" },
+                css = { "prettierd" },
+                html = { "prettierd" },
+                json = { "prettierd" },
+                yaml = { "prettierd" },
+                lua = { "stylua" },
+                python = { "isort", "black" }
+            }
+        })
+
+        vim.keymap.set({ "n", "v" }, "<F3>", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500
+            })
+        end)
+    end
+}
