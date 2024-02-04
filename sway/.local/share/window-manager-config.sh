7b612e3ca0c75daafc0889b42fea409d4ed3644b
Andreas Kvist <kvistcake@gmail.com>
2024-02-04 11:13:07 +0100

    Set some more stuff if using nvidia

--- sway/.local/share/window-manager-config.sh
+++ sway/.local/share/window-manager-config.sh
@@ -2,3 +2,10 @@ export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export QT_QPA_PLATFORM=wayland
 export SWAY_CONFIG_FOLDER="$HOME/.config/sway"
+
+
+if command -v nvidia-settings &> /dev/null; then
+    export WLR_RENDERER=vulkan
+    export WLR_NO_HARDWARE_CURSORS=1
+    export XWAYLAND_NO_GLAMOR=1
+fi
