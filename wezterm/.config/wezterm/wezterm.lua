b28eaf9fd9053743a8ae51f7158750a54d203af5
Andreas Kvist <kvistcake@gmail.com>
2024-04-03 15:35:27 +0200

    More shit. Wezterm as alacritty fucking died?

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+return {
+    color_scheme = "Catppuccin Mocha",
+    window_background_opacity = 0.9,
+    font_size = 11.0,
+    enable_tab_bar = false,
+    window_padding = {
+        bottom = 8,
+        left = 8,
+        right = 8,
+        top = 8,
+    }
+}
