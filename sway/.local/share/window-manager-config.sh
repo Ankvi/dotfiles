f175c8ae31f9f5941c45a43a84f9fe1e4b3b9d0b
Andreas Kvist <kvistcake@gmail.com>
2024-05-30 08:22:33 +0200

    Sway: QT6 and removed some nvidia stuff as it'll never happen

--- sway/.local/share/window-manager-config.sh
+++ sway/.local/share/window-manager-config.sh
@@ -1,11 +1,6 @@
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME=qt6ct
 export SWAY_CONFIG_FOLDER="$HOME/.config/sway"
-
-
-if command -v nvidia-smi &> /dev/null; then
-    export WLR_RENDERER=vulkan
-    export WLR_NO_HARDWARE_CURSORS=1
-    export XWAYLAND_NO_GLAMOR=1
-fi
+export ELECTRON_OZONE_PLATFORM_HINT=auto
